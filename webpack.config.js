10bc70fc049fec0109f9d65ee1e5695f8ba2f49b
David Geene <david.geene@gmail.com>
2018-09-26 08:40:36 -0400

    resolve babel webpack compilation. project now builds. need to resolve file copying

--- webpack.config.js
+++ webpack.config.js
@@ -1,14 +1,24 @@
-var path = require('path');
-
+var path = require("path");
 
 module.exports = {
-    entry: './src/index.js',
-    output: {
-        filename: 'js/bundle.js',
-        path: path.resolve(__dirname, 'dist'),
-        publicPath: 'dist' // used when webpack-dev-server has public ip. allows reloading
-    },
-    devServer: {
-        disableHostCheck: true // security risk. I use it when i'm devloping on a headless machine
-    }
+  entry: "./src/index.js",
+  output: {
+    filename: "js/bundle.js",
+    path: path.resolve(__dirname, "dist"),
+    publicPath: "dist" // used when webpack-dev-server has public ip. allows reloading
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: "babel-loader"
+        }
+      }
+    ]
+  },
+  devServer: {
+    disableHostCheck: true // security risk. I use it when i'm devloping on a headless machine
+  }
 };
